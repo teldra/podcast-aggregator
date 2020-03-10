a608b99c11ae875ee16324c5ddd024b999c02cf9
teldra <teldra@rotce.de>
2020-03-10 14:57:16 +0100

    podcast-aggregator: fix indentation

--- podcast-aggregator.py
+++ podcast-aggregator.py
@@ -11,7 +11,7 @@ if len (sys.argv) <= 1 :
 	print("Usage: python $URL ")
 	sys.exit (1)
 
-print("<rss xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\" version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">")
+print("<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">")
 print("<channel>")
 print("<title></title>")
 print("<link></link>")
@@ -31,14 +31,14 @@ for i in range(len(sys.argv)):
 			enclosure = ep['enclosures'][0]
 			epurl = enclosure['url'].split("?")[0]
 
-print("<item>")
-print("<title>%s: %s</title>" % (feedtitle.replace('&', '&amp;'),eptitle.replace('&', '&amp;')))
-print("<description><![CDATA[%s\n\n%s\n\n<a href=\"%s\">%s</a>\n\n<a href=\"%s\">%s</a>\n]]></description>" % (epdesc, feeddesc, epurl, epurl, feedlink, feedlink))
-print("<guid>%s</guid>" % (epurl))
-print("<pubDate>%s UTC</pubDate>" % (eppubdate))
-print("<enclosure url=\"%s\"/>" % (epurl))
-print("</item>")
-print("\n")
+			print("<item>")
+			print("<title>%s: %s</title>" % (feedtitle.replace('&', '&amp;'),eptitle.replace('&', '&amp;')))
+			print("<description><![CDATA[%s\n\n%s\n\n<a href=\"%s\">%s</a>\n\n<a href=\"%s\">%s</a>\n]]></description>" % (epdesc, feeddesc, epurl, epurl, feedlink, feedlink))
+			print("<guid>%s</guid>" % (epurl))
+			print("<pubDate>%s UTC</pubDate>" % (eppubdate))
+			print("<enclosure url=\"%s\"/>" % (epurl))
+			print("</item>")
+			print("\n")
 
 print("</channel>")
 print("</rss>")
